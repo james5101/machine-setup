c0cec8eb90a009cbc8dcb5d852b2f3adac3b17c4
James Noonan <james.c.noonan@gmail.com>
2019-02-28 10:15:00 -0500

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -23,7 +23,7 @@ function Install-ChocoPackages {
 
     Install-Choco
 
-    $Packages = 'kubernetes-cli','kubernetes-helm', 'vscode', 'terraform', 'azure-cli'
+    $Packages = 'kubernetes-cli','kubernetes-helm', 'vscode', 'terraform', 'azure-cli', 'git'
 
     ForEach ($PackageName in $Packages)
     {
@@ -34,7 +34,7 @@ function Install-ChocoPackages {
 
 function Upgrade-ChocoPackages {
     
-    $Packages = 'kubernetes-cli','kubernetes-helm', 'vscode', 'terraform', 'azure-cli'
+    $Packages = 'kubernetes-cli','kubernetes-helm', 'vscode', 'terraform', 'azure-cli', 'git'
 
     ForEach ($PackageName in $Packages)
     {
