8e92030f59dbe4dff47e4ceb3311bc6f51634f51
james5101 <james.c.noonan@gmail.com>
2019-09-18 21:09:39 -0500

    add some more devel tools

--- centos/init.sh
+++ centos/init.sh
@@ -1,5 +1,20 @@
 yum groupinstall -y "development tools"
 yum install -y lsof wget vim-enhanced words which
 
+yum install -y \
+  libffi-devel \
+  zlib-devel \
+  bzip2-devel \
+  openssl-devel \
+  ncurses-devel \
+  sqlite-devel \
+  readline-devel \
+  tk-devel \
+  gdbm-devel \
+  db4-devel \
+  libpcap-devel \
+  xz-devel \
+  expat-devel
+
 #set up bashrc
 $ curl https://raw.githubusercontent.com/linuxacademy/content-python3-sysadmin/master/helpers/bashrc -o ~/.bashrc
